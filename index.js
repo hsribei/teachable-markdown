3d1d4f27ff3f5419d9acc613743d3aa6c7a7b88f
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-11-29 02:02:02 -0300

    Use built html instead of rendering md on client

--- index.js
+++ index.js
@@ -1,57 +1,53 @@
-const unified = require("unified");
-const markdown = require("remark-parse");
-const remark2rehype = require("remark-rehype");
-const format = require("rehype-format");
-const html = require("rehype-stringify");
-const raw = require("rehype-raw");
-
-const md2html = unified()
-  .use(markdown)
-  .use(remark2rehype, { allowDangerousHTML: true })
-  .use(raw)
-  .use(format)
-  .use(html).processSync;
-
 const isLocalhost = window.location.href.startsWith("http://localhost:");
 
-function renderMarkdown(markdownText) {
+function renderHtml(htmlText) {
   const parentTag = document.querySelector(".lecture-content");
 
-  let markdownDiv = document.getElementById("markdown");
-  if (!markdownDiv) {
-    markdownDiv = document.createElement("div");
-    markdownDiv.id = "markdown";
-    parentTag.appendChild(markdownDiv);
+  let htmlDiv = document.getElementById("html");
+  if (!htmlDiv) {
+    htmlDiv = document.createElement("div");
+    htmlDiv.id = "html";
+    parentTag.appendChild(htmlDiv);
 
     const styleTag = document.createElement("style");
     styleTag.type = "text/css";
     styleTag.innerHTML = `
-      #markdown img {
+      #html figure {
         display: block;
         max-width: 80%;
         margin: auto;
+        margin-block-start: 1em;
+        margin-block-end: 1em;
+      }
+
+      #html figcaption:before {
+        content: "Caption: ";
+      }
+
+      #html img {
+        width: 100%;
       }
     `;
     document.getElementsByTagName("head")[0].appendChild(styleTag);
   }
 
-  markdownDiv.innerHTML = String(md2html(markdownText));
+  htmlDiv.innerHTML = htmlText;
 }
 
 function getUrl() {
   let url = "";
 
   if (isLocalhost) {
-    url = "http://localhost:5000/full-gfm.md";
+    url = "http://localhost:5000/full-html.html";
   } else {
     try {
       // const docTitle = "s01e02 - Third Lecture | test-school";
       const docTitle = document.title;
       const sectionDirectoryName = docTitle.match(/^(s\d+)e/)[1];
       const lectureFileName = encodeURIComponent(
-        docTitle.split(/\s*\|\s*/)[0] + ".md"
+        docTitle.split(/\s*\|\s*/)[0] + ".html"
       );
-      url = `https://cdn.staticaly.com/gh/hsribei/content/master/teachable-gfm-markdown/${sectionDirectoryName}/${lectureFileName}?env=dev`;
+      url = `https://cdn.staticaly.com/gh/hsribei/content/master/teachable-html/${sectionDirectoryName}/${lectureFileName}?env=dev`;
     } catch (e) {
       console.log(
         "Couldn't parse document title into URL for markdown source:",
@@ -66,16 +62,17 @@ function getUrl() {
   return url;
 }
 
-async function fetchAndRender() {
+async function fetchBody() {
   const url = getUrl();
   const response = await fetch(url);
-  const markdownText = await response.text();
-  renderMarkdown(markdownText);
+  const body = await response.text();
+  return body;
 }
 
 (async function main() {
   try {
-    await fetchAndRender();
+    const body = await fetchBody();
+    renderHtml(body);
   } catch (e) {
     console.error(e);
   }
